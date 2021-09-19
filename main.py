48eea7a25c12c582aadf296e6a63442f2d67c18d
joconnor02 <jmoconnor02@gmail.com>
2021-09-19 15:44:47 -0400

    Week 3 Assignment

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+name = input("What is your name?")
+print(name)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+num = int(input("Please input a number"))
+num2 = num * num
+print(num2)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('I guess that all I have to do for week one?')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+word = input("Please input a word")
+print(len(word))
