4e60ce46a2696d158093599a34abc21eb72b7dbd
joconnor02 <jmoconnor02@gmail.com>
2021-09-26 19:49:27 -0400

    Week 4 Assignment

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-name = input("What is your name?")
-print(name)
+import random
 
-num = int(input("Please input a number"))
-num2 = num * num
-print(num2)
+"""
+array = [
+        ['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']
+        ]
 
-word = input("Please input a word")
-print(len(word))
+for x in range(len(array[0])):
+    for y in range(len(array)):
+        print(array[y][x], end="")
+    print("\n")
+
+"""
+
+userint = input("Make a selection(0/1)")
+randint = random.randint(0, 1)
+print(randint)
+if userint == randint:
+    print("You won!")
+else:
+    print("The computer won.")
